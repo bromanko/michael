a01fb9b61495e3a896f191c48bb8cb7401041629
Brian Romanko <hello@bromanko.com>
2026-01-30 13:50:26 -0800

    fix: use default nixfmt package and apply formatter fixes to treefmt.nix

--- treefmt.nix
+++ treefmt.nix
@@ -1,10 +1,8 @@
-{ pkgs, ... }:
-{
+_: {
   projectRootFile = "flake.nix";
 
   programs.nixfmt = {
     enable = true;
-    package = pkgs.nixfmt-rfc-style;
     includes = [
       "*.nix"
       "nix/**/*.nix"
